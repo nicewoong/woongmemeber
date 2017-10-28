68ab17e82bbc5548dcd7dd9dc61bf1c33b651ad0
nicewoong <thoughtscode@gmail.com>
2017-10-28 21:31:23 +0900

    Completely added dialog at MainListAdpater when item cliked

--- src/main/java/com/aurelhubert/ahbottomnavigation/demo/mainlist/MainListRecyclerAdapter.java
+++ src/main/java/com/aurelhubert/ahbottomnavigation/demo/mainlist/MainListRecyclerAdapter.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +12,41 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.aurelhubert.ahbottomnavigation.demo.R;
 import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
+import com.github.javiersantos.materialstyleddialogs.enums.Duration;
 
 import java.io.File;
 import java.util.ArrayList;
 
 /**
- *
+ * 메인 명함리스트 (MainListFragment) 에서 리스트의 조작을 담당할 recyclerViewAdapter 이다
+ * Apache License 오픈소스 라이브러리인 MaterialStyledDialogs 를 사용하였다
+ * Copyright 2016 Javier Santos
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ MaterialStyledDialogs includes code from material-dialogs, which is
+ licensed under the MIT license. You may obtain a copy at
+
+ see https://github.com/afollestad/material-dialogs/blob/master/LICENSE.txt
+
  */
 public class MainListRecyclerAdapter extends RecyclerView.Adapter<MainListRecyclerAdapter.ViewHolder> {
 
 	private ArrayList<BusinessCardProfile> businessCardProfiles = new ArrayList<BusinessCardProfile>();
-	BusinessCardProfile currentCard ;
 
 	public static class ViewHolder extends RecyclerView.ViewHolder {
 		public TextView nameTextView;
@@ -64,11 +87,12 @@ public class MainListRecyclerAdapter extends RecyclerView.Adapter<MainListRecycl
 	// 그리고 여기서 아이템 순서에 있는 데이터가 바인더 되니까 이곳에서 온클릭리스너를 구현해주면 되겠다
 	@Override
 	public void onBindViewHolder(ViewHolder holder, int position) {
-		currentCard = businessCardProfiles.get(position);
+		final BusinessCardProfile currentCard = businessCardProfiles.get(position);
 		holder.nameTextView.setText(businessCardProfiles.get(position).name); //이름 지정
 		holder.roleTextView.setText(businessCardProfiles.get(position).role); // 직책 지정
 		holder.companyTextView.setText(businessCardProfiles.get(position).company); // 회사 이름 지정
 		holder.businessCardImageView.setImageResource(businessCardProfiles.get(position).imageDrawableID);
+
 		holder.itemView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -78,8 +102,33 @@ public class MainListRecyclerAdapter extends RecyclerView.Adapter<MainListRecycl
 						.setDescription(currentCard.role + "\n" + currentCard.company )
 						.setHeaderDrawable(currentCard.imageDrawableID)
 						.withDarkerOverlay(true) // 배경화면 오버레이
-						.withIconAnimation(true) // 애니메이션 추가
+						.withDialogAnimation(true, Duration.SLOW) // 다이얼로그 나타나고 사라질 때 애니메이션
+						.setCancelable(true) // 바깥 눌렀을 때 다이얼로그 삭제
+						.setPositiveText("명함보기")
+						.onPositive( new MaterialDialog.SingleButtonCallback() {
+							@Override
+							public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+							}
+						})
+						.setNeutralText("대화하기")
+						.onNeutral( new MaterialDialog.SingleButtonCallback() {
+							@Override
+							public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+							}
+						})
+						.setNegativeText("이 명함 전달")
+						.onNegative( new MaterialDialog.SingleButtonCallback() {
+							@Override
+							public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+
+							}
+						})
 						.show();
+
+
+
 			}
 		});
 

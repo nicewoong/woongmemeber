9e572bf65a906968e2c8966a10bdd4cbc59c3401
nicewoong <thoughtscode@gmail.com>
2017-10-28 18:40:39 +0900

    Added Sample Data to message fragment list

--- src/main/java/com/aurelhubert/ahbottomnavigation/demo/message/MessageFragment.java
+++ src/main/java/com/aurelhubert/ahbottomnavigation/demo/message/MessageFragment.java
@@ -1,28 +1,20 @@
 package com.aurelhubert.ahbottomnavigation.demo.message;
 
 import android.content.Context;
-import android.database.DataSetObserver;
+
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.aurelhubert.ahbottomnavigation.demo.R;
-import com.aurelhubert.ahbottomnavigation.demo.lounge.LoungeRecyclerAdapter;
+
 import com.yydcdut.sdlv.Menu;
 import com.yydcdut.sdlv.MenuItem;
 import com.yydcdut.sdlv.SlideAndDragListView;
@@ -31,6 +23,9 @@ import java.util.ArrayList;
 
 /**
  * 세 번째 탭인 메시지 탭에 대한 프래그먼트
+ * SlideAndDragListView 라는 라이브러리를 사용하여
+ * 리스트 아이템의 좌우 슬라이드시 메뉴 아이콘 기능을 구현하였다.
+ * see : https://github.com/yydcdut/SlideAndDragListView
  */
 public class MessageFragment extends Fragment {
 
@@ -62,18 +57,14 @@ public class MessageFragment extends Fragment {
 
 
     public void initView(View view) {
-        // Construct the data source
-        ArrayList<User> arrayOfUsers = new ArrayList<User>();
-        arrayOfUsers.add(new User("KCJ","111111111"));
-        arrayOfUsers.add(new User("HWJ","222222222"));
 
-// Create the adapter to convert the array to views
-        UsersAdapter adapter = new UsersAdapter(this.getContext(), arrayOfUsers);
-// Attach the adapter to a ListView
+        // List 를 관리할 Adapter 를 생성한다
+        UsersAdapter adapter = new UsersAdapter(this.getContext(), getSampleUsersData());
+        // Attach the adapter to a ListView
         SlideAndDragListView slideAndDragListView = (SlideAndDragListView) view.findViewById(R.id.slide_and_drag_list_view);
         slideAndDragListView.setMenu(createMenu());
-        slideAndDragListView.setAdapter(adapter);
-        addMenuItemClickListener(slideAndDragListView);
+        slideAndDragListView.setAdapter(adapter); // 생성한 ArrayAdapter<User> 를 이용해서 리스트뷰에 표시하기 위해 어뎁터를 지정한다
+        addMenuItemClickListener(slideAndDragListView); // 리스트 아이템에 슬라이드 메뉴를 추가한다
     }
 
 
@@ -136,8 +127,30 @@ public class MessageFragment extends Fragment {
     }
 
 
+    /**
+     * 현재 official 한 데이터가 없는 관계로
+     * 채팅하고 있는 상대방 사용자 샘플 데이터 리스트를 구성하여 반환한다.
+     * @return 샘플 User데이터 (이름과 직책 정보를 담는다.)
+     */
+    public ArrayList<User> getSampleUsersData() {
 
-    //========== Adapter ============== //
+        ArrayList<User> arrayOfUsers = new ArrayList<User>();
+        arrayOfUsers.add(new User("한웅제", "학생 (경북대학교 컴퓨터학부)"));
+        arrayOfUsers.add(new User("김말숙", "대리 (한국감정원 정보전산실/ 정보개발)"));
+        arrayOfUsers.add(new User("김정태", "대리 (신용보증기금 기획실)"));
+        arrayOfUsers.add(new User("신상보", "대표 (동복 해녀 회국수)"));
+        arrayOfUsers.add(new User("정도현", "사회복지사 (대구장애인종합복지관)"));
+        arrayOfUsers.add(new User("James", "Associate Director (Charles W.Davidson Corp.)"));
+        arrayOfUsers.add(new User("Amy", "Senior Data Engineer (NETFLIX data Engineering Center)"));
+
+
+        return arrayOfUsers;
+
+    }
+
+
+    //============================== Adapter ============================== //
+
     private class UsersAdapter extends ArrayAdapter<User> {
 
 
@@ -176,6 +189,7 @@ public class MessageFragment extends Fragment {
     /**
      * 리스트 아이템의 데이터를 넣을 용도로 사용되는 User 객체
      * user 정보를 담고 있다
+     * 초 간단한 객체이므로 내부에 위치하였다. 추후 객체가 복잡하고 커지면 외부 클래스로 두는 것으로 하자
      */
     private class User {
         public String name;

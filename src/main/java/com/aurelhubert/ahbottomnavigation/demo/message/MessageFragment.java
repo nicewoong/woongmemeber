b9aadabab231d721dd9713d0aaada21abbc80154
nicewoong <thoughtscode@gmail.com>
2017-10-28 18:28:37 +0900

    Completed SlideMenu Function and Added Listener on SliedMenu at message list item

--- src/main/java/com/aurelhubert/ahbottomnavigation/demo/message/MessageFragment.java
+++ src/main/java/com/aurelhubert/ahbottomnavigation/demo/message/MessageFragment.java
@@ -19,6 +19,7 @@ import android.widget.FrameLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aurelhubert.ahbottomnavigation.demo.R;
 import com.aurelhubert.ahbottomnavigation.demo.lounge.LoungeRecyclerAdapter;
@@ -72,26 +73,66 @@ public class MessageFragment extends Fragment {
         SlideAndDragListView slideAndDragListView = (SlideAndDragListView) view.findViewById(R.id.slide_and_drag_list_view);
         slideAndDragListView.setMenu(createMenu());
         slideAndDragListView.setAdapter(adapter);
+        addMenuItemClickListener(slideAndDragListView);
     }
 
 
+    /**
+     * 리스트 아이템을 좌우로 슬라이드 했을 때 메뉴 나타나기 기능
+     * 반환되는 객체를 slide list 에 추가할 것이다
+     * @return
+     */
     public Menu createMenu() {
 
         Menu menu = new Menu(true, 0);//the first parameter is whether can slide over
-        menu.addItem(new MenuItem.Builder().setWidth(90)//set Width
-                .setBackground(new ColorDrawable(Color.RED))// set background
-                .setText("One")//set text string
+
+        // 오른쪽으로 슬라이드 했을 때 즐겨찾기 아이콘 추가
+        menu.addItem(new MenuItem.Builder().setWidth(120)//set Width
+                .setBackground(new ColorDrawable(Color.WHITE))// set background
                 .setTextColor(Color.GRAY)//set text color
                 .setTextSize(20)//set text size
-                .setIcon(getResources().getDrawable(R.drawable.ic_apps_black_24dp))// set icon
+                .setIcon(getResources().getDrawable(R.drawable.favourite_50))// set icon
                 .build());
+
+        // 왼쪽으로 슬라이드 했을 때 삭제하기 아이콘 추가
         menu.addItem(new MenuItem.Builder().setWidth(120)
-                .setBackground(new ColorDrawable(Color.BLACK))
+                .setBackground(new ColorDrawable(Color.YELLOW))
                 .setDirection(MenuItem.DIRECTION_RIGHT)//set direction (default DIRECTION_LEFT)
-                .setIcon(getResources().getDrawable(R.drawable.ic_maps_local_bar))// set icon
+                .setIcon(getResources().getDrawable(R.drawable.ic_delete_50))// set icon
                 .build());
         return menu;
 
+    }
+
+
+    /**
+     * 리스트 아이템을 좌우로 슬라이드 했을 때 나타나는 메뉴를 클릭했을 때의 리스너를 구현
+     */
+    public void addMenuItemClickListener(SlideAndDragListView slideAndDragListView) {
+        slideAndDragListView.setOnMenuItemClickListener(new SlideAndDragListView.OnMenuItemClickListener() {
+            @Override
+            public int onMenuItemClick(View v, int itemPosition, int buttonPosition, int direction) {
+                switch (direction) {
+                    case MenuItem.DIRECTION_LEFT: // 슬라이드 해서 왼쪽 메뉴 아이콘 눌렀을 때
+                        switch (buttonPosition) {
+                            case 0://One
+                                Toast.makeText(getActivity(), "즐겨찾기 합니다.", Toast.LENGTH_SHORT).show();
+                                return Menu.ITEM_SCROLL_BACK;
+                        }
+                        break;
+                    case MenuItem.DIRECTION_RIGHT:
+                        switch (buttonPosition) { // 슬라이드 해서 오른쪽 메뉴 아이콘 눌렀을 때
+                            case 0://icon
+                                Toast.makeText(getActivity(), "삭제합니다.", Toast.LENGTH_SHORT).show();
+                                return Menu.ITEM_DELETE_FROM_BOTTOM_TO_TOP;
+                        }
+                        break;
+                    default :
+                        return Menu.ITEM_NOTHING;
+                }
+                return Menu.ITEM_NOTHING;
+            }
+        });
     }
 
 
@@ -102,12 +143,16 @@ public class MessageFragment extends Fragment {
 
         public UsersAdapter(Context context, ArrayList<User> users) {
             super(context, 0, users);
-
-
         }
 
 
-
+        /**
+         * 리스트에서 보일 아이템 뷰를 생성하고 해당 뷰에 데이터를 집어넣는다
+         * @param position
+         * @param convertView
+         * @param parent
+         * @return
+         */
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             // Get the data item for this position
@@ -128,6 +173,10 @@ public class MessageFragment extends Fragment {
     }
 
 
+    /**
+     * 리스트 아이템의 데이터를 넣을 용도로 사용되는 User 객체
+     * user 정보를 담고 있다
+     */
     private class User {
         public String name;
         public String role;
